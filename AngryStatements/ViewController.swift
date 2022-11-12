e4bf4bc2488500f5dd1dedb14341afdef80e5201
BorisKiselev <hugh.dancy@icloud.com>
2022-11-12 15:58:17 +0700

    Initial Commit

---
+++ AngryStatements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AngryStatements
+//
+//  Created by Борис Киселев on 12.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
